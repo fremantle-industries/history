b816b3cb7e497c2f72dfa5539489f5c70930798b
Alex <alex+git@fremantle.io>
2021-08-21 23:18:55 -0700

    Remove funding rate history job/chunk dirs

--- lib/history/funding_rate_history_chunks/funding_rate_history_chunk_status_type.ex
+++
@@ -1,5 +0,0 @@
-defmodule History.FundingRateHistoryChunks.FundingRateHistoryChunkStatusType do
-  use EctoEnum,
-    type: :funding_rate_history_chunk_status_type,
-    enums: [:enqueued, :error, :working, :not_found, :complete]
-end
--- lib/history/funding_rate_history_jobs/funding_rate_history_job_status_type.ex
+++
@@ -1,5 +0,0 @@
-defmodule History.FundingRateHistoryJobs.FundingRateHistoryJobStatusType do
-  use EctoEnum,
-    type: :funding_rate_history_job_status_type,
-    enums: [:enqueued, :error, :working, :complete]
-end
