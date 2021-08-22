14c014e24564bba9f7c2355e20c6792b93b3fb6e
Alex <alex+git@fremantle.io>
2021-08-21 23:20:25 -0700

    Remove lending rate history job/chunk dirs

--- lib/history/lending_rate_history_chunks/lending_rate_history_chunk_status_type.ex
+++
@@ -1,5 +0,0 @@
-defmodule History.LendingRateHistoryChunks.LendingRateHistoryChunkStatusType do
-  use EctoEnum,
-    type: :lending_rate_history_chunk_status_type,
-    enums: [:enqueued, :error, :working, :complete]
-end
