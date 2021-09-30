dba5714b1049a8d403a74be0863cc3dc262dcf46
alex <alex+git@fremantle.io>
2021-09-29 20:56:44 -0700

    Release v0.0.18

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule History.MixProject do
   def project do
     [
       app: :history,
-      version: "0.0.17",
+      version: "0.0.18",
       elixir: "~> 1.11",
       package: package(),
       elixirc_paths: elixirc_paths(Mix.env()),
